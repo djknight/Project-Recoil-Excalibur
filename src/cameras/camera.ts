ef03a609fe69aaf7cbddff5170f902aae4315ebe
DJ.Knight <djknight90@outlook.com>
2022-09-20 11:25:56 +1000

    camera Idears added

---
+++ src/cameras/camera.ts
@@ -0,0 +1,11 @@
+//radious normal
+game.currentScene.camera.strategy.radiusAroundActor(Actor, radius);
+
+//camera shake radious to exolotion?
+Camera.shake;
+
+//zoom with mouse wheel
+Camera.zoom;
+
+// press key to unlock camera look around
+// free camera folow mouse?
