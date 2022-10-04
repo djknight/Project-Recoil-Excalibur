6ec9c259cee990ba5bb9a80c06822ba7ac7d62f1
DJ.Knight <djknight90@outlook.com>
2022-10-04 19:06:29 +1000

    added Ammo stats

---
+++ src/projectiles/ammoLog.ts
@@ -0,0 +1,14 @@
+//Ammo-Weapons Log
+const Shell = {
+  //shell
+  ShellAp: 10,
+  ShellDmg: 10,
+  shellSpeed: +1000,
+  setTimeout: 1000,
+
+  //rocket
+  rocketAoe: 20,
+  rocketDmg: 10,
+  rocketSpeed: +800,
+  rocket_setTimeout: 1500,
+};
