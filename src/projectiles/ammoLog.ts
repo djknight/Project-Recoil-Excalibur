faba4177c6e64c8cb8f83e43c9e0644842613d40
Daniel Knight <djknight90@outlook.com>
2022-10-07 11:15:02 +1000

    ammolog update

--- src/projectiles/ammoLog.ts
+++ src/projectiles/ammoLog.ts
@@ -5,10 +5,23 @@ const Shell = {
   ShellDmg: 10,
   shellSpeed: +1000,
   setTimeout: 1000,
-
+};
+const rocket = {
   //rocket
   rocketAoe: 20,
   rocketDmg: 10,
   rocketSpeed: +800,
-  rocket_setTimeout: 1500,
+  // rocket_setTimeout: 1500,
+  setTimeout: 1500,
 };
+
+const missile = {
+  //missile
+  missileSpeed: +1200,
+  missileDmg: 30,
+  setTimeout: 10000,
+};
+
+/// since const use normal Time out and Dmg?
+//is speed also the same?/
+//is setTimeout?
